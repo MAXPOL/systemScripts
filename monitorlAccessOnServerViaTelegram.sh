74b097bc3a64966daae33f09a36184357de72b42
MAXPOL <maxrol32@gmail.com>
2020-11-15 20:00:49 +0300

    Script to monitor access to the server via telegram

---
+++ monitorlAccessOnServerViaTelegram.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+user="$(whoami)"
+
+date="$(date)"
+
+curl -s -X POST https://api.telegram.org/bot<YOU_KEY>/sendMessage -d chat_id=<YOU_CHAT_ID> -d text="Access to the web server <YOU_SERVICE_NAME> via user: $user in $date ."
+
