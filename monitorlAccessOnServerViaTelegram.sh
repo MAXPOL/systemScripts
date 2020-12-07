a54afa63c3753e4b3b2a6264290daad3f7e831da
MAXPOL <maxrol32@gmail.com>
2020-12-07 13:29:51 -0500

    modified: Change monitorlAccessOnServerViaTelegram.sh

--- monitorlAccessOnServerViaTelegram.sh
+++ monitorlAccessOnServerViaTelegram.sh
@@ -6,3 +6,4 @@ date="$(date)"
 
 curl -s -X POST https://api.telegram.org/bot<YOU_KEY>/sendMessage -d chat_id=<YOU_CHAT_ID> -d text="Access to the web server <YOU_SERVICE_NAME> via user: $user in $date ."
 
+exit
